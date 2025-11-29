fbbff5c8d9644832b152143fc3ae94b8e8571662
Petr Marek <petr.marek18@gmail.com>
2025-11-29 11:00:46 +0100

    fix: colors in constants

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
     './screens/**/*.{js,ts,tsx}',
     './navigation/**/*.{js,ts,tsx}',
     './i18n/**/*.{js,ts,tsx}',
+    './constants/**/*.{js,ts,tsx}',
   ],
 
   presets: [require('nativewind/preset')],
@@ -13,18 +14,18 @@ module.exports = {
     extend: {
       colors: {
         red: {
-          50:  '#fdebec',   // very light red, but not pink
+          50: '#fdebec', // very light red, but not pink
           100: '#fbd6d9',
           200: '#f5adb3',
           300: '#ec7a84',
           400: '#e3475a',
-          500: '#c71a2b',   // FRESH BLOOD TONE (dominant crimson)
+          500: '#c71a2b', // FRESH BLOOD TONE (dominant crimson)
           600: '#a91524',
           700: '#8c111e',
           800: '#6e0d17',
-          900: '#4f0911',   // coagulated blood depth
+          900: '#4f0911', // coagulated blood depth
           950: '#34060c',
-        }
+        },
       },
     },
   },
